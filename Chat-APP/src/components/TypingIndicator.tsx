233184d66a271d55a4e3238ce5fbcac42c66134f
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-09-07 10:11:19 +0500

    Code : TypingIndicator component

--- Chat-APP/src/components/TypingIndicator.tsx
+++ Chat-APP/src/components/TypingIndicator.tsx
@@ -1,9 +1,34 @@
-export default function TypingIndicator() {
+import React from 'react';
+
+interface TypingIndicatorProps {
+  users: string[];
+}
+
+export const TypingIndicator: React.FC<TypingIndicatorProps> = ({ users }) => {
+  if (users.length === 0) {
+    return null;
+  }
+
+  const renderTypingText = () => {
+    if (users.length === 1) {
+      return `${users[0]} is typing...`;
+    } else if (users.length === 2) {
+      return `${users[0]} and ${users[1]} are typing...`;
+    } else {
+      return `${users[0]} and ${users.length - 1} others are typing...`;
+    }
+  };
+
   return (
     <div className="typing-indicator">
-      <span></span>
-      <span></span>
-      <span></span>
+      <span className="typing-text">{renderTypingText()}</span>
+      <div className="typing-dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   );
-}
+};
+
+export default TypingIndicator;
