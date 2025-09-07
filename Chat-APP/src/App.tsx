bbd3b67ffaa7f79db99f0b377e36dd2005a22b1f
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-09-07 09:55:08 +0500

    Code: App.tsx

---
+++ Chat-APP/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Vite + React!</h1>
+      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </div>
+  )
+}
+
+export default App;
