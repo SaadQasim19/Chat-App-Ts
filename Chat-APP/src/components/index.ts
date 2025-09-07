c41e380989619dcede609973d9119a1de920b506
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-09-07 10:04:21 +0500

    Code : exporter of all components

---
+++ Chat-APP/src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as ChatHeader } from './ChatHeader';
+export { default as MessageInput } from './MessageInput';
+export { default as MessageItem } from './MessageItem';
+export { default as Sidebar } from './Sidebar';
+export { default as TypingIndicator } from './TypingIndicator';
+export { default as ThemeToggle } from './ThemeToggle';
+export { default as HamburgerMenu } from './HamburgerMenu';
+export { default as MessageStatus } from './MessageStatus';
