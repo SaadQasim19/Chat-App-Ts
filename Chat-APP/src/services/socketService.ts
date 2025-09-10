d54b83deba2207d23b062a10e60c3770d160cf92
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-09-10 14:42:31 +0500

    Code : Socket Service --> Message Events

--- Chat-APP/src/services/socketService.ts
+++ Chat-APP/src/services/socketService.ts
@@ -4,13 +4,18 @@ import type {Message , User} from "../types";
 
 export interface SocketEvents{
 
+//! Message events
+  'message:send': (data: { conversationId: string; message: Omit<Message, 'id' | 'timestamp'> }) => void;
+  'message:receive': (data: { conversationId: string; message: Message }) => void;
+  'message:delivered': (data: { conversationId: string; messageId: string }) => void;
+  'message:read': (data: { conversationId: string; messageId: string }) => void;
   
 
-class SocketService {
+// class SocketService {
 
 
 
-}
+// }
 
 
 }
